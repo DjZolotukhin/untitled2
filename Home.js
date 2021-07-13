2d684a68c0b7c81f3d428bc89d0f515b762dd839
DJ ZOLOTUKHIN <dj.zolotukhin@gmail.com>
2021-07-13 19:34:57 +0300

    create htrf

---
+++ Home.js
@@ -0,0 +1,146 @@
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14,
+//     2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log , alert, document.write
+// - Переприсвоїти кожну змінну з завдання вище на будь які довільні значення.
+//     Вивести кожну змінну за допомогою: console.log , alert, document.write
+
+
+// let a='hello';
+// a='Hi';
+// console.log(a);
+// document.write(a);
+// alert(a);
+//
+// let b='owu';
+// b='Okten';
+// console.log(b);
+// document.write(b);
+// alert(b);
+//
+// let c='com';
+// c='in';
+// console.log(c);
+// document.write(c);
+// alert(c);
+//
+// let d='ua';
+// d='Lviv';
+// console.log(d);
+// document.write(d);
+// alert(d);
+//
+// let e=1;
+// e=2;
+// console.log(e);
+// document.write(e);
+// alert(e);
+
+// let f=10;
+// f=21;
+// console.log(f);
+// document.write(f);
+// alert(f);
+//
+// let G=-999;
+// g=-888;
+// console.log(g);
+// document.write(g);
+// alert(g);
+
+// let j=123;
+// j=234;
+// console.log(j);
+// document.write(j);
+// alert(j);
+//
+// let h=3.14;
+// h='Pi';
+// console.log(h);
+// document.write(h);
+// alert(h);
+//
+// let k=2.7;
+// k=2.34;
+// console.log(k);
+// document.write(k);
+// alert(k);
+//
+// 16;
+// true;
+// false;
+
+
+// ClassWork
+// 1. Оголосіть змінні str, num, flag и txt зі значеннями «Привіт», 123, true, «true».
+// За допомогою typeof переконайтесь, що значення змінних належать типам: string, number и  boolean.
+
+// let str = 'jnvrg';
+// console.log(typeof str);
+//
+// let num=123;
+// console.log(typeof num);
+//
+// let flag = true;
+// console.log(typeof flag);
+//
+// let txt = 5>3;
+// console.log(typeof txt)
+
+
+// 2. Оголосіть змінні a1, a2, a3, a4, a5. За домогою 3х математичних оператцій отримайте числа:
+//         34,
+//         12,
+//         66,
+//         90,
+//         87
+// Старайтесь використовувати різні оператори.
+//     Example: 88 = (16 / 2) * 11
+
+// let a=28, b=13, c=7;
+// console.log(a+b-c);
+
+// let a=2, b=5, c=2;
+// console.log(a+b*c)
+
+// let a=88, b=2, c=22;
+// console.log(a/b+c);
+
+// let a=33, b=2, c=999;
+// console.log(a**b-c);
+
+// let a=15, b=3, c=32;
+// console.log(a*b+c);
+
+
+// 3.  Створіть змінні a6, a7, a8, a9, a10. Запишіть в них результати виразів:
+//     5 % 3,
+//     3 % 5,
+//     5 + '3',
+//     '5' - 3,
+//     75 + 'кг'
+
+
+//  let a6 = 5 % 3;
+//  console.log(a6);
+//
+//  let a7 = 3 % 5;
+//  console.log(a7);
+//
+//  let a8 = 5 + '3';
+//  console.log(a8);
+//
+//
+// let a9 = '5' - 3;
+// console.log(a9);
+//
+// let a10 = 75 + 'кг';
+// console.log(a10);
+
+// 4. Напишіть код, який вираховує площу прямокутника висотою 23см. (змінна height) та шириною 10см (змінна width).
+// Значееня площі зберігати в змінній s.
+
+// let height=23, width=10;
+// area = (height*width);
+// console.log(area);
+
+
