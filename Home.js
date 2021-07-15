fe511d138062d21bcbc3f77089fcc69944d0b00a
DjZolotukhin <dj.zolotukhin@gmail.com>
2021-07-15 13:28:49 +0300

    Home15.07

--- Home.js
+++ Home.js
@@ -1,242 +1,62 @@
-// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14,
-//     2.7, 16, true, false.
-//     Вивести кожну змінну за допомогою: console.log , alert, document.write
-// - Переприсвоїти кожну змінну з завдання вище на будь які довільні значення.
-//     Вивести кожну змінну за допомогою: console.log , alert, document.write
+// Використовуючи данні з масиву, за допомоги document.write та циклу
+// побудувати структуру по шаблону template1.1
+
+let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
+document.write('<ul>');
+for (let list0 of listOfItems){
+    document.write(`<li>${list0}</li>`);
+
+}
+document.write('</ul>');
 
 
-// let a='hello';
-// a='Hi';
-// console.log(a);
-// document.write(a);
-// alert(a);
-//
-// let b='owu';
-// b='Okten';
-// console.log(b);
-// document.write(b);
-// alert(b);
-//
-// let c='com';
-// c='in';
-// console.log(c);
-// document.write(c);
-// alert(c);
-//
-// let d='ua';
-// d='Lviv';
-// console.log(d);
-// document.write(d);
-// alert(d);
-//
-// let e=1;
-// e=2;
-// console.log(e);
-// document.write(e);
-// alert(e);
-
-// let f=10;
-// f=21;
-// console.log(f);
-// document.write(f);
-// alert(f);
-//
-// let G=-999;
-// g=-888;
-// console.log(g);
-// document.write(g);
-// alert(g);
-
-// let j=123;
-// j=234;
-// console.log(j);
-// document.write(j);
-// alert(j);
-//
-// let h=3.14;
-// h='Pi';
-// console.log(h);
-// document.write(h);
-// alert(h);
-//
-// let k=2.7;
-// k=2.34;
-// console.log(k);
-// document.write(k);
-// alert(k);
-//
-// 16;
-// true;
-// false;
+// Використовуючи данні з масиву, за допомоги document.write та циклу
+// побудувати структуру по шаблону template2.1 & template2.2. Зробити адекватну стилізацію.
+//     Великими літерами прописанні властивості об'єкту які потрібно впровадити в шаблон.
+// Шаблон застосувати до кожного об'єкта в масиві
 
 
 
-// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. Зконкатенувати їх в одну змінну person.
-// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
-//     Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
-
-//
-// let person = {
-//     firstName: 'Соляревич',
-//     middleName: 'Микола',
-//     lastName: 'Марянович'
-//
-// }
-
-// let firstName = 'Соляревич';
-// let middleName = 'Микола';
-// let lastName = 'Марянович';
-// let person = firstName + middleName + lastName;
-// console.log(person);
-
-// let firstName = prompt ('Вітаю твоє імя');
-// let middleName = prompt('По батькові');
-// let lastName = prompt('Твій вік');
-
-// За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
-//     let a = 100; let b = '100'; let c = true;
-
-// let a = 100;
-// let b = '100';
-// let c = true;
-// console.log(typeof a);
-// console.log(typeof b);
-// console.log(typeof c);
-
-// - Поставет відповідний оператор в виразах що б вийшов відповідний результат.
-//     В однакових виразаї не використовувати однакові оператори!
-//     5 ? 6 -> true
-// 5 ? 6 -> false
-// 5 ? 6 -> false
-// 5 ? 6 -> false
-// 10 ? 10 -> true
-// 10 ? 10 -> true
-// 10 ? 10 -> false
-// 10 ? 10 -> false
-// 10 ? 10 -> false
-// 123 ? '123' -> false
-// 123 ? '123' -> true
-// console.log(5<6);
-// console.log(5>6);
-// console.log(5==6);
-// console.log(5===6);
-// console.log(10=10);
-// console.log(10===10);
-// console.log(10<10);
-// console.log(10>10);
-// console.log(10!=10);
-// console.log(123>'123');
-// console.log(123=='123');
-
-
-
-
-
-
-// ClassWork
-// 1. Оголосіть змінні str, num, flag и txt зі значеннями «Привіт», 123, true, «true».
-// За допомогою typeof переконайтесь, що значення змінних належать типам: string, number и  boolean.
-
-// let str = 'Hello';
-// console.log(typeof str);
-//
-// let num=123;
-// console.log(typeof num);
-//
-// let flag = true;
-// console.log(typeof flag);
-//
-// let txt = 5>3;
-// console.log(typeof txt)
-
-
-// 2. Оголосіть змінні a1, a2, a3, a4, a5. За домогою 3х математичних оператцій отримайте числа:
-//         34,
-//         12,
-//         66,
-//         90,
-//         87
-// Старайтесь використовувати різні оператори.
-//     Example: 88 = (16 / 2) * 11
-
-// let a=28, b=13, c=7;
-// console.log(a+b-c);
-
-// let a=2, b=5, c=2;
-// console.log(a+b*c)
-
-// let a=88, b=2, c=22;
-// console.log(a/b+c);
-
-// let a=33, b=2, c=999;
-// console.log(a**b-c);
-
-// let a=15, b=3, c=32;
-// console.log(a*b+c);
-
-
-// 3.  Створіть змінні a6, a7, a8, a9, a10. Запишіть в них результати виразів:
-//     5 % 3,
-//     3 % 5,
-//     5 + '3',
-//     '5' - 3,
-//     75 + 'кг'
-
-
-//  let a6 = 5 % 3;
-//  console.log(a6);
-//
-//  let a7 = 3 % 5;
-//  console.log(a7);
-//
-//  let a8 = 5 + '3';
-//  console.log(a8);
-//
-//
-// let a9 = '5' - 3;
-// console.log(a9);
-//
-// let a10 = 75 + 'кг';
-// console.log(a10);
-
-// 4. Напишіть код, який вираховує площу прямокутника висотою 23см. (змінна height) та шириною 10см (змінна width).
-// Значееня площі зберігати в змінній s.
-
-// let height=23, width=10;
-// area = (height*width);
-// console.log(area);
-
-// 5.  Напиши код, который находит объем цилиндра высотой 10м (переменная heightC) и диаметром основания 4м (dC),
-//     результат поместите в переменную v.
-
-// let hc=10, dc=4, pi=3.14;
-// v = (hc*dc*pi);
-// console.log(v);
-
-
-// 6. У прямоугольного треугольника две стороны n (со значением 3) и m (со значением 4).
-// Найдите гипотенузу k по теореме Пифагора (нужно использовать функцию Math.pow(число, степень) или оператор возведения в степень ** ).
-
-// let n=3, m=4;
-// k = (Math.sqrt(n**2 + m**2));
-// console.log(k);
-
-// 8. Вывести в окно браузера при помощи метода alert() следующие данные: Ваше ФИО, возраст, хобби (каждой на новой строки спомощью \n).
-
-// let name = ('Як вас звати', 'ggughjkb');
-// alert('Ваше імя' + name);
-// let age = 36;
-// alert('Вам' + age);
-// let hobby = 'музика';
-// alert('Ваше хобі' + hobby);
-
-// 9. Создать 4 переменные с использованием ключевого слова let с именами str1, str2, str3, concatenation.
-//     Переменной str1 присвоить фразу ‘Кто ‘, str2 – ‘ты ‘, str3 – ‘такой?’
-//   Локальной переменной concatenation присвоить результат конкатенации 3-х строк: str1, str2, str3.
-//     Вывести в документ содержимое переменной concatenation спомощью document.write
-
-// let str1 = 'Хто';
-// let str2 = 'ти';
-// let str3 = 'такий';
-// concatenation = str1 + ' ' + str2 + ' ' + str3;
-// document.write(concatenation);
+let simpsons = [
+    {
+        name: 'Bart',
+        surname: 'Simpson',
+        age: 10,
+        info: 'Бартолом\'ю ДжоДжо «Барт» Сімпсон (англ. Bartholomew JoJo «Bart» Simpson) — один із головних героїв мультиплікаційного серіалу Сімпсони. Барт — найстарша дитина Гомера і Мардж Сімпсон. У нього також є дві молодші сестри — Ліса і Меґґі. Барт є втіленням образу бешкетника та посереднього учня у школі. Разом зі своїм батьком Барт є одним із найвідоміших персонажів у цьому серіалі.',
+        photo: 'https://upload.wikimedia.org/wikipedia/uk/a/aa/Bart_simpson.png'
+    },
+    {
+        name: 'Homer',
+        surname: 'Simpson',
+        age: 40,
+        info: 'Гомер Джей Сімпсон (англ. Homer Jay Simpson) — один із головних героїв мультсеріалу «Сімпсони». Гомер — грубий і неввічливий батько родини, він має очевидні вади: товстий, лисий і не дуже розумний. Нерідко він поводиться як блазень, абсурдно, егоїстично і нетактовно, але все ж лишається симпатичним.',
+        photo: 'http://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png'
+    },
+    {
+        name: 'Marge',
+        surname: 'Simpson',
+        age: 38,
+        info: 'Ма́рджори Жакли́н «Мардж» Си́мпсон (в девичестве Бувье́) (англ. Marjorie Jacqueline «Marge» Simpson) — постоянный персонаж мультипликационного сериала «Симпсоны», её озвучивает Джулия Кавнер. Обычно носит зелёное платье, красные балетки, на шее — ожерелье из искусственного жемчуга и ездит на оранжевом универсале. У неё шикарные синие волосы, которые она обычно собирает в очень высокую причёску. Глаза цвета ореха (19s6e). Основное занятие — домохозяйка, большую часть времени проводит в заботе о доме, детях и Гомере. Образ Мардж копирует стереотип провинциальной американской домохозяйки 50-х годов. Мардж — единственный член семьи, посещающий церковь добровольно. Старается поддерживать нравственность не только своей семьи, но и всего города. Отлично готовит, особенно славятся её свиные отбивные и зефир. Любимое блюдо — лапша с маслом.',
+        photo: 'https://upload.wikimedia.org/wikipedia/ru/0/0b/Marge_Simpson.png'
+    },
+    {
+        name: 'Lisa',
+        surname: 'Simpson',
+        age: 9,
+        info: 'Ли́за Мари́ Си́мпсон (англ. Lisa Marie Simpson) — героиня мультипликационного сериала «Симпсоны». Средний ребёнок в семье, восьмилетняя девочка, выделяющаяся среди остальных Симпсонов прежде всего своим умом и рассудительностью.',
+        photo: 'https://upload.wikimedia.org/wikipedia/ru/e/ec/Lisa_Simpson.png'
+    },
+    {
+        name: 'Maggie',
+        surname: 'Simpson',
+        age: 1,
+        info: 'Ма́ргарет Эвелин «Мэ́гги» Си́мпсон (англ. Margaret Evelyn “Maggie” Simpson) — персонаж мультсериала «Симпсоны». Впервые появилась на телевидении в шоу Трейси Ульман, в короткометражке Good Night (англ.)русск. 19 апреля 1987 года. Мэгги была придумана и разработана карикатуристом Мэттом Грейнингом, пока он ждал встречи с Джеймсом Л. Бруксом. Названа в честь младшей сестры Грейнинга. После появления в шоу Трейси Ульман, через три года семья Симпсонов получила собственный сериал на телеканале Fox, дебют произошёл 17 декабря 1989 года.',
+        photo: 'https://upload.wikimedia.org/wikipedia/ru/9/9d/Maggie_Simpson.png'
+    },
+];
+document.write(`<div>`);
+for (const simpson of simpsons){
+    document.write(`<div>${simpson.name} - ${simpson.surname} - ${simpson.age} - ${simpson.info} -
+ <img src="${simpson.photo}" alt=""></img> </div>`);
+}
+        document.write(`</div>`);
