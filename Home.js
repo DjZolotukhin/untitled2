ce9647ab820bd07f958057aca310f604c7047561
DjZolotukhin <dj.zolotukhin@gmail.com>
2021-07-28 10:47:19 +0300

    Home27.07.v6.2

--- Home.js
+++ Home.js
@@ -44,20 +44,41 @@
 // https://jsonplaceholder.typicode.com/posts
 //     зробити кнопку до кожного поста. при кліку на яку виводяться в окремий блок всі коментарі поточного поста
 
+//
+// fetch('https://jsonplaceholder.typicode.com/posts')
+//     .then(response => response.json())
+//     .then(posts => {
+//         let postsBox = document.getElementsByClassName('posts-box')[0];
+//         for (const post of posts){
+//             let pPost = document.createElement('p');
+//             pPost.innerText = `${post.userId} - ${post.id} - ${post.title}`;
+//             let detailsBtn = document.createElement('button');
+//             detailsBtn.innerText = 'details';
+//             detailsBtn.onclick = function (){
+//                fetch(`https://jsonplaceholder.typicode.com/posts/${post.id}/comments`)
+//                    .then(value => value.json())
+//                    .then(comments =>{
+//                        let commentsBox = document.getElementsByClassName('comments-box')[0];
+//                        commentsBox.innerHTML = '';
+//                        for (const comment of comments) {
+//                            let liComment = document.createElement('li');
+//                            liComment.innerText = comment.body;
+//                            commentsBox.appendChild(liComment);
+//
+//                        }
+//                    });
+//             }
+//                         pPost.appendChild(detailsBtn);
+//                         postsBox.appendChild(pPost);
+//         }
+//     });
 
-fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(response => response.json())
-    .then(posts => {
-        let postsBox = document.getElementsByClassName('posts-box')[0];
-        for (const post of posts){
-            let pPost = document.createElement('p');
-            pPost.innerText = `${post.postId}`;
-            let detailsBtn = document.createElement('button');
-            detailsBtn.innerText = 'details';
-            detailsBtn.onclick = function (){
-                console.log('print posts of user', post.postId)
-            }
-                        pPost.append(detailsBtn);
-                        postsBox.append(pPost);
-        }
-    })
+
+// ===============================================================================
+
+// additional
+
+// Отримати відповідь з цього ресурсу відповідь, та вивести в документ як в прикладі на занятті
+// https://jsonplaceholder.typicode.com/users
+//     кожному елементу юзера створити кнопку, при клику на яку в окремий блок виводяться всі пости поточного юзера.
+//     Кожному елементу post створити кнопку, при клику на яку в окремий блок виводяться всі коментарі поточного поста
